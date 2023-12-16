058d45983b8016f60b10a9d3f15f85599530178f
kyruv <kylerruvane@gmail.com>
2023-12-15 23:52:21 -0700

    allow exporting sqlite db from admin

--- Django/cairo/app/admin.py
+++ Django/cairo/app/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from app.models import User
+from import_export.admin import ExportActionMixin
 
-class MyAdmin(admin.ModelAdmin):
+class MyAdmin(ExportActionMixin, admin.ModelAdmin):
     readonly_fields = ('created_at','updated_at',)
 
 admin.site.register(User, MyAdmin)
