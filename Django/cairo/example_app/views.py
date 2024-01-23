e2fb789f9e6e6e203b1ab2964676f4e8807d109a
kyruv <kylerruvane@gmail.com>
2024-01-23 03:53:20 -0500

    the pre-survey is the one who makes the DB entry

--- Django/cairo/example_app/views.py
+++ Django/cairo/example_app/views.py
@@ -22,20 +22,9 @@ def unity_callback_game_end(request):
 
     print(f"user_id: {user_id}, attempt: {attempt_number}, game_progress: {game_progress}, num_clicks: {num_clicks}")
     
-    db_entry, did_create = FlappyBirdStudyResult.objects.get_or_create(user_id=user_id)
+    db_entry, _ = FlappyBirdStudyResult.objects.get_or_create(user_id=user_id)
 
-    if did_create:
-        db_entry.game_data = {
-            'game': [{
-                'clicks': num_clicks,
-                'progess': game_progress
-            }]
-        }
-    else:
-        print(db_entry)
-        print(db_entry.game_data)
-        print(type(db_entry.game_data))
-        print(db_entry.game_data['game'])
+    if 'game' in db_entry.game_data:
         existing_data = list(db_entry.game_data['game'])
         existing_data.append({
                 'clicks': num_clicks,
@@ -43,6 +32,13 @@ def unity_callback_game_end(request):
             })
         db_entry.game_data = {
             'game': existing_data
+        }
+    else:
+        db_entry.game_data = {
+            'game': [{
+                'clicks': num_clicks,
+                'progess': game_progress
+            }]
         }
     
     db_entry.save()
@@ -54,11 +50,7 @@ def pre_game_survey_data(request):
     json_data = json.loads(request.body.decode('utf-8'))
     user_id = json_data.get('user_id')
 
-    db_entry, did_create = FlappyBirdStudyResult.objects.get_or_create(user_id=user_id)
-
-    if did_create:
-        return HttpResponse("Database Entry didn't exist even though it should have")
-    
+    db_entry, _ = FlappyBirdStudyResult.objects.get_or_create(user_id=user_id)
     db_entry.s1_name = json_data.get('name')
     db_entry.prolific_id = json_data.get('prolific_id')
     db_entry.s1_color = json_data.get('color')
